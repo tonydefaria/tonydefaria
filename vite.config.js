d6d5ea3ccd142f0216c4c96f7d15cdbb59054c19
tonydefaria <me@tonydefaria.com>
2025-05-25 10:01:59 +0200

    test cdn

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? process.env.ASSET_CDN_URL + "/" : "/",
+  base: process.env.APP_ENV === "production" ? process.env.ASSET_CDN_URL + "/" : "/",
   plugins: [
     laravel({
       input: ["resources/css/site.scss", "resources/js/site.js"],
