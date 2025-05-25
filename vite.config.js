400520f9b1110fc18695686215dfa8cdc417c8e6
tonydefaria <me@tonydefaria.com>
2025-05-25 10:12:28 +0200

    trying to get cdn to work

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
 
-export default defineConfig({
-  base: process.env.APP_ENV === "production" ? process.env.ASSET_CDN_URL + "/" : "/",
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? process.env.ASSET_CDN_URL + "/" : "/",
   plugins: [
     laravel({
       input: ["resources/css/site.scss", "resources/js/site.js"],
       refresh: true
     })
   ]
-});
+}));
